3db6c034dda6d1dfaf5082004d898d69512280dc
NPS team <JelinJose.Jose@barclayscapital.com>
2016-05-11 09:11:10 -0400

    Made symlink pointer distinct

--- formatter/formatter.go
+++ formatter/formatter.go
@@ -79,7 +79,7 @@ func (f *Formatter) Next() (string, error) {
 		// Show symlink target
 		if f.ShowSymlinkTarget() {
 			if n.IsSymlink() {
-				text += " -➢ " + n.SymlinkTarget()
+				text += " —‒► " + n.SymlinkTarget()
 			}
 		}
 	}
